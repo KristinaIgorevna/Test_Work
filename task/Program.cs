8b8ccfd9e2eb8d18a88e68ea8e377176b1d7ab7f
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-08-10 12:02:19 +0400

    работает

--- task/Program.cs
+++ task/Program.cs
@@ -19,7 +19,7 @@ void FormMass(string[] array1, string[] array2)
 
 void Print(string[] array)
 {
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
